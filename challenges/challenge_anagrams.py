92ff5a8dcc4d3bcb9ef6569f5c347942c0f6074f
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-09-01 18:13:44 -0300

    feat(is_anagram): implementado função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,30 @@
+def partition(str, start, end):
+    delimiter = start - 1
+
+    for index in range(start, end):
+        if str[index] <= str[end]:
+            delimiter = delimiter + 1
+            str[index], str[delimiter] = (
+                str[delimiter],
+                str[index],
+            )
+
+    str[delimiter + 1], str[end] = str[end], str[delimiter + 1]
+
+    return delimiter + 1
+
+
+def quick_sort(str, start, end):
+    if start < end:
+        part = partition(str, start, end)
+        quick_sort(str, start, part - 1)
+        quick_sort(str, part + 1, end)
+
+
 def is_anagram(first_string, second_string):
-    pass
+    str_first_sorted = list(first_string.lower())
+    str_second_sorted = list(second_string.lower())
+    quick_sort(str_first_sorted, 0, len(str_first_sorted) - 1)
+    quick_sort(str_second_sorted, 0, len(str_second_sorted) - 1)
+
+    return str_first_sorted == str_second_sorted
