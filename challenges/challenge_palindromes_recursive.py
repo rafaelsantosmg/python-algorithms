ac5951be3a8ecef7dd4c0f5a4070cdfd0b336bd3
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-09-01 17:23:39 -0300

    feat(is_palindrome_recursive): Implementada a função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if not word:
+        return False
+
+    while low_index <= high_index:
+        if word[high_index] == word[low_index]:
+            high_index -= 1
+            low_index += 1
+            is_palindrome_recursive(word, low_index, high_index)
+        else:
+            return False
+    return True
